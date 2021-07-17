e451f20d065ec1c0eec21b4ec8fe6fc5765fb8d9
pontakornth <pontakorn61@gmail.com>
2021-07-17 17:29:43 +0700

    feat: Add editing feature

--- src/components/TheEditor.tsx
+++ src/components/TheEditor.tsx
@@ -4,6 +4,16 @@ import styles from './TheEditor.module.css'
 
 const TheEditor = () => {
 	const noteState = useNotes();
+	function handleChangeTitle(e: React.ChangeEvent<HTMLHeadingElement>) {
+		const currentNote = noteState.notes[noteState.currentNoteIndex]
+		console.log("edi")
+		noteState.editNote({...currentNote, title: e.target.innerHTML})
+	}
+
+	function handleChangeContent(e: React.ChangeEvent<HTMLDivElement>){
+		const currentNote = noteState.notes[noteState.currentNoteIndex]
+		noteState.editNote({...currentNote, content: e.target.innerHTML})
+	}
 	return (
 	<div className={styles.theEditor}>
 		<div className="tabs">
@@ -11,8 +21,18 @@ const TheEditor = () => {
 			<div className="tab">Preview</div>
 		</div>
 		<div className={styles.editorContent}>
-			<h2>{noteState.notes[noteState.currentNoteIndex].title}</h2>
-			{noteState.notes[noteState.currentNoteIndex].content}
+			<h2 
+				dangerouslySetInnerHTML={{__html: noteState.notes[noteState.currentNoteIndex].title}}
+				onBlur={handleChangeTitle}
+				contentEditable={true}
+			>
+			</h2>
+			<div className="content"
+			    dangerouslySetInnerHTML={{__html: noteState.notes[noteState.currentNoteIndex].content}}
+				onBlur={handleChangeContent}
+				contentEditable={true}
+			>
+			</div>
 		</div>
 	</div>
 )}
