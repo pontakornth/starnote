bc582c8620e502fa1cf8519b94d0ceca3e2a08e0
pontakornth <pontakorn61@gmail.com>
2021-07-18 14:23:05 +0700

    feat: install open sans

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import ViteFonts from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    ViteFonts({
+      google: {
+        families: [ 
+          {
+            name: 'Open Sans',
+            styles: 'ital,wght@0,400;0,700;1,400;1,700'
+          } 
+         ]
+      }
+    })
+  ]
 })
