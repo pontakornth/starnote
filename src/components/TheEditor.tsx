c5006d05940cc01f1c4a901c8c0b599b47faf8d1
pontakornth <pontakorn61@gmail.com>
2021-07-20 16:01:01 +0700

    feat: Implement syntax highlighter

--- src/components/TheEditor.tsx
+++ src/components/TheEditor.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 import useNotes from '../stores/note'
 import styles from './TheEditor.module.css'
 import ReactMarkdown from 'react-markdown'
-import { note } from 'vfile-message'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import {vscDarkPlus as dark} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { NormalComponents, SpecialComponents } from 'react-markdown/src/ast-to-react'
+
+const components: Partial<NormalComponents & SpecialComponents> = {
+	code({node, inline, className, children, ...props}) {
+	const match = /language-(\w+)/.exec(className || '')
+		return !inline && match ? (
+		<SyntaxHighlighter style={dark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+		) : (
+		<code className={className} {...props}>
+			{children}
+		</code>
+		)
+	}
+}
 
 const TheEditor = () => {
 	const noteState = useNotes();
@@ -44,7 +59,7 @@ const TheEditor = () => {
 			) : (
 				<>
 				<h1>{noteState.notes[noteState.currentNoteIndex].title}</h1>
-				<ReactMarkdown>
+				<ReactMarkdown components={components}>
 					{noteState.notes[noteState.currentNoteIndex].content}
 				</ReactMarkdown>
 				</>
